f6adfc229e09a83f8d9679c0ce498dd12019110e
Kenneth Walker <kewalker@uvic.ca>
2020-10-26 16:55:26 -0700

    Add project files.

---
+++ ArcGISAppPointRendering/ArcGISAppPointRendering.iOS/Main.cs
@@ -0,0 +1,12 @@
+﻿using UIKit;
+
+namespace ArcGISAppPointRendering
+{
+    public class Application
+    {
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
---
+++ ArcGISAppPointRendering/ArcGISAppPointRendering/MapPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Portal;
+using Esri.ArcGISRuntime.Security;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ArcGISAppPointRendering
+{
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+            Startup();
+        }
+
+        private async void Startup()
+        {
+            var item = await PortalItem.CreateAsync(new Uri("https://latitudegeo.maps.arcgis.com/home/item.html?id=2655d105a5944426a4c48c044b93fdd1"));
+            TheMap.Map = new Map(item);
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var item = await PortalItem.CreateAsync(new Uri("https://latitudegeo.maps.arcgis.com/home/item.html?id=2655d105a5944426a4c48c044b93fdd1"));
+            TheMap.Map = new Map(item);
+        }
+    }
+}
